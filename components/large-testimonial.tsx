be377aa37cdbde5c892a6b5edcd17e423bfe95bb
yash_kumar <yash9704.iitd@gmail.com>
2025-02-14 17:40:20 +0530

    testimonial heading changed

--- components/large-testimonial.tsx
+++ components/large-testimonial.tsx
@@ -4,7 +4,13 @@ import TestimonialImg from "@/public/images/large-testimonial.jpg";
 export default function LargeTestimonial() {
   return (
     <section className="mb-[200px] px-4">
-      <h1 className="text-5xl font-bold text-center text-blue-500 mt-8">Testimonial</h1>
+      <h2 className="text-5xl font-bold text-center text-blue-500 mt-8">What users say...</h2>
+      <h3 className="text-lg font-semibold text-center text-grey-400 mt-2 md:text-3xl">150+ user interviews, reviews, and shoutouts—and counting 
+      </h3>
+      <h3 className="text-base font-medium text-center text-green-500 mt-1 md:text-4xl">Driven by user-first thesis 
+
+</h3>
+
 
       <div className="mt-12 grid grid-cols-1 md:grid-cols-3 gap-6">
         {["togRbicQWEQ", "YS8wIpw01w4", "bfR0Q7dyhJo"].map((videoId, index) => (
