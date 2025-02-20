b226b9f8324d7081e74b84c2f3cf47f7b92c6dc7
Gaurav <65115466+gauravyad86@users.noreply.github.com>
2025-02-20 18:26:14 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   optimizeFonts: true,
-}
+  async rewrites() {
+    return [
+      {
+        source: "/api/counter",
+        destination: "http://65.0.207.184:4001/api/counter",
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
